8bd29eece4417052cf095b7172fae1c7d17363a6
Kateryna Strough <katerynastrough@hotmail.com>
2019-06-13 22:51:31 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,7 +1,14 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle 
   
+<<<<<<< HEAD
  def go
     "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
+=======
+  def go 
+    
+  end 
+  
+>>>>>>> a5be4baa022d98a3de14331070c1b70ae9ede6cc
 end 
